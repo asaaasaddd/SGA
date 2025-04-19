0b3d0960e4cd103768e1ba508a87ced9c0ccd03a
AndyL <126047934+asaaasaddd@users.noreply.github.com>
2025-04-19 20:36:41 +0800

    Update refine.py

--- refine.py
+++ refine.py
@@ -309,9 +309,9 @@ def add_edges_based_on_candidates(source_A_hat, target_A_hat, seed_list1, seed_l
                     src_neighbors_edges_union = get_neighbors(source_A_hat, src_i) | get_neighbors(source_A_hat, src_j)
                     tgt_neighbors_edges_union = {seed_dict_source_to_target.get(n) for n in src_neighbors_edges_union if n in seed_dict_source_to_target}
                     tgt_neighbors_target = get_neighbors(target_A_hat, tgt_i) | get_neighbors(target_A_hat, tgt_j)
-                    max_edges = len(tgt_neighbors_edges_union & tgt_neighbors_target)
-                    consis_edges = len(tgt_neighbors_edges_union | tgt_neighbors_target)
-                    neiborhood_consistency = max_edges / consis_edges if consis_edges != 0 else 0
+                    consistent_edges = len(tgt_neighbors_edges_union & tgt_neighbors_target)
+                    max_edges = len(tgt_neighbors_edges_union | tgt_neighbors_target)
+                    neiborhood_consistency = consistent_edges / max_edges if max_edges != 0 else 0
                     if neiborhood_consistency >= threshold_u:
                         target_A_hat[tgt_i, tgt_j] = 1
                         target_A_hat[tgt_j, tgt_i] = 1
@@ -327,9 +327,9 @@ def add_edges_based_on_candidates(source_A_hat, target_A_hat, seed_list1, seed_l
                     tgt_neighbors_edges_union = get_neighbors(target_A_hat, tgt_i) | get_neighbors(target_A_hat, tgt_j)
                     src_neighbors_edges_union = {seed_dict_target_to_source.get(n) for n in tgt_neighbors_edges_union if n in seed_dict_target_to_source}
                     src_neighbors_source = get_neighbors(source_A_hat, src_i) | get_neighbors(source_A_hat, src_j)
-                    max_edges = len(src_neighbors_edges_union & src_neighbors_source)
-                    consis_edges = len(src_neighbors_edges_union | src_neighbors_source)
-                    neiborhood_consistency = max_edges / consis_edges if consis_edges != 0 else 0
+                    consistent_edges = len(src_neighbors_edges_union & src_neighbors_source)
+                    max_edges = len(src_neighbors_edges_union | src_neighbors_source)
+                    neiborhood_consistency = consistent_edges / max_edges if max_edges != 0 else 0
                     if neiborhood_consistency >= threshold_u:
                         source_A_hat[src_i, src_j] = 1
                         source_A_hat[src_j, src_i] = 1
